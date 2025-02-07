509acb1ba61c044abe4f49360535fe0e3e6c0566
Suraj <Surajsinghnagrkoti2992003@gmail.com>
2025-02-07 22:34:05 +0530

    Added the files

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(express.json());
+const PORT = 1790;
+
+app.use(cors());
+
+// Route to fetch images from Unsplash
+app.get("/api/images", async (req, res) => {
+  try{
+    const place = req.query.place;
+    if (!place) return res.status(400).json({ error: "Place name is required" });
+
+    const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: { query: place, per_page: 5, client_id: accessKey },
+    });
+
+    const images = response.data.results.map((image) => ({
+      url: image.urls.regular,
+    }));
+    res.json(images);
+  } catch(error){
+    console.error("Error fetching images:", error);
+    res.status(500).json({ error: "Failed to fetch images" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
